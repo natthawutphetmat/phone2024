46280fa3e44da0198644713b8e5ed8ab96072bf1
natthawutphetmat <167544607+natthawutphetmat@users.noreply.github.com>
2024-09-28 18:00:08 +0700

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -21,6 +21,31 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
+      <script
+      dangerouslySetInnerHTML={{
+        __html: `
+          !function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(window, document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', '1869279590229240');
+          fbq('track', 'PageView');
+        `,
+      }}
+    />
+    {/* NoScript Tag สำหรับกรณีที่เบราว์เซอร์ปิดการทำงานของ JavaScript */}
+    <noscript>
+      <img
+        height="1"
+        width="1"
+        style={{ display: 'none' }}
+        src="https://www.facebook.com/tr?id=1869279590229240&ev=PageView&noscript=1"
+      />
+    </noscript>
       <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"/>
       </head>
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
